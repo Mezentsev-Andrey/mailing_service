6e95816899187e78f7849f36d2a2208061d5ee20
Andrey <mezentsew@mail.ru>
2024-04-24 19:28:56 +0300

    Added new changed information.

--- users/urls.py
+++ users/urls.py
@@ -11,32 +11,16 @@ from users.views import (
     get_users_list,
 )
 
-app_name = 'users'
+app_name = UsersConfig.name
 
 urlpatterns = [
     path("", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path(
-        "register/",
-        RegisterView.as_view(template_name="users/register.html"),
-        name="register",
-    ),
-    path(
-        "verify_message/",
-        TemplateView.as_view(template_name="users/verify_message.html"),
-        name="verify_message",
-    ),
+    path("register/", RegisterView.as_view(template_name="users/register.html"), name="register"),
+    path("verify_message/", TemplateView.as_view(template_name="users/verify_message.html"), name="verify_message"),
     path("email/verify/<str:verify_code>", verification, name="verify"),
-    path(
-        "success_verify/",
-        TemplateView.as_view(template_name="users/success_verify.html"),
-        name="success_verify",
-    ),
-    path(
-        "invalid_verify/",
-        TemplateView.as_view(template_name="users/invalid_verify.html"),
-        name="invalid_verify",
-    ),
+    path("success_verify/", TemplateView.as_view(template_name="users/success_verify.html"), name="success_verify"),
+    path("invalid_verify/", TemplateView.as_view(template_name="users/invalid_verify.html"), name="invalid_verify"),
     path("users_list/", get_users_list, name="list_view"),
     path("edit/<int:pk>", UserUpdateView.as_view(), name="edit"),
 ]
